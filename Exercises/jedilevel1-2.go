6899ff1d411642600d98469fd32ab168dab47d1c
kunal <kunalvijay93@gmail.com>
2020-10-06 20:37:49 +1300

    jedilevel1-2.go complete

---
+++ Exercises/jedilevel1-2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+// The compiler assigned values to the variables.
+// These variables are called ZERO values
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
